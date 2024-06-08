aa5c84467052b7f5fdc9de35f44c5c8bd25d0078
krislao <krislao13@gmail.com>
2024-06-08 21:35:21 +0800

    feat: add site top navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {},
+    screens: {
+      xs: "425px",
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 };
