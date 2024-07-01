1743741220693700e96f08b159cf30abf3cc9328
krislao <krislao13@gmail.com>
2024-07-01 16:45:41 +0800

    feat: make experience list responsive on browser resize

--- index.js
+++ index.js
@@ -1,50 +1,85 @@
 (function () {
-  var experience = document.querySelector("#experience");
-  var experienceList = experience.querySelector("ul");
+  var experienceSection = document.querySelector("#experience");
+  var showAllButton = document.querySelector("button[data-toggle='collapse']");
+  var experienceList = document.querySelector(
+    `#${showAllButton.dataset.target}`,
+  );
+
+  var lastItem = experienceList.querySelector("li:last-of-type");
   var lastShownItem = experienceList.querySelector("li:nth-of-type(2)");
   var firstHiddenItem = lastShownItem.nextElementSibling;
 
-  var showAllButton = experience.querySelector("button");
   var showAllContent = showAllButton.querySelector("span:first-of-type");
   var showLessContent = showAllContent.nextElementSibling;
 
-  var expanded = experienceList.offsetHeight;
-  var collapsed = lastShownItem.offsetTop + lastShownItem.offsetHeight;
+  var expanded;
+  var collapsed;
 
   var experienceListStyle = experienceList.style;
   var showLessContentStyle = showLessContent.style;
   var showAllContentStyle = showAllContent.style;
 
-  // default values:
-  // 1. list collapsed
-  // 2. show last 2 recent entries
-  // 3. show "Show All"
-  experienceListStyle.height = `${collapsed}px`;
-  showLessContentStyle.display = "none";
+  initialize();
+  collapse();
+
+  function initialize() {
+    expanded = experienceList.scrollHeight;
+    collapsed = lastShownItem.offsetTop + lastShownItem.clientHeight;
+  }
+
+  function expand() {
+    showAllButton.ariaExpanded = "true";
+    experienceListStyle.height = `${expanded}px`;
+    showAllContentStyle.display = "none";
+    showLessContentStyle.display = null;
+  }
+
+  function collapse() {
+    showAllButton.ariaExpanded = "false";
+    experienceListStyle.height = `${collapsed}px`;
+    showAllContentStyle.display = null;
+    showLessContentStyle.display = "none";
+  }
+
+  function throttle(callback, delay) {
+    var throttled = false;
+    return function () {
+      if (!throttled) {
+        throttled = true;
+        callback();
+        setTimeout(function () {
+          throttled = false;
+        }, delay);
+      }
+    };
+  }
 
   showAllButton.addEventListener("click", function () {
-    if (experienceList.dataset.expanded === undefined) {
-      experienceList.dataset.expanded = "";
-      experienceListStyle.height = `${expanded}px`;
-
-      showAllContentStyle.display = "none";
-      showLessContentStyle.display = null;
+    if (this.ariaExpanded === "false") {
+      expand();
 
       setTimeout(function () {
         firstHiddenItem.scrollIntoView();
-      }, 400);
+      }, 300);
     } else {
-      delete experienceList.dataset.expanded;
-      experienceListStyle.height = `${collapsed}px`;
-
-      showAllContentStyle.display = null;
-      showLessContentStyle.display = "none";
+      collapse();
 
       setTimeout(function () {
-        experience.scrollIntoView();
-      }, 400);
+        experienceSection.scrollIntoView();
+      }, 300);
     }
   });
 
-  // TODO: recalibrate on resize
+  window.addEventListener(
+    "resize",
+    throttle(function () {
+      initialize();
+
+      if (showAllButton.ariaExpanded === "true") {
+        experienceListStyle.height = `${lastItem.offsetTop + lastItem.clientHeight + 8}px`; // +8 for bottom padding
+      } else {
+        collapse();
+      }
+    }, 400),
+  );
 })();
